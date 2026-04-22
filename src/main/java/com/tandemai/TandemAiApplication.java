f583718979d5d2c0c48682d47491a5e06393b418
Javed Bagwan <javed460@gmail.com>
2026-04-22 07:26:44 +0530

    added TandemAiApplication

---
+++ src/main/java/com/tandemai/TandemAiApplication.java
@@ -0,0 +1,12 @@
+package com.tandemai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TandemAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TandemAiApplication.class, args);
+    }
+}
